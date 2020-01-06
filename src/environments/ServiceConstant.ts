1d5af3546ff4ba2e773db35d45ff19e12bccba9a
susilpanda <susilchand@gmail.com>
2020-01-06 21:45:51 +0800

    updated url

--- src/environments/ServiceConstant.ts
+++ src/environments/ServiceConstant.ts
@@ -1,5 +1,5 @@
 export class ServiceConstant {
-    public static HOST:string = "http://localhost:8080";
-    //public static HOST: string = "http://" + document.location.hostname;
+    //public static HOST:string = "http://localhost:8080";
+    public static HOST: string = "http://" + document.location.hostname;
    
   }
